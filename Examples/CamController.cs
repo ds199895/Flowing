45a6e066f6546658816c6411f30dc2827b9e0547
He Siyuan <71424814+ds199895@users.noreply.github.com>
2022-10-28 01:21:03 +0800

    Add files via upload

---
+++ Examples/CamController.cs
@@ -0,0 +1,264 @@
+﻿using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flowing
+{
+    class CamController
+    {
+        IApp app;
+        public GameWindow window;
+        public int w;
+        public int h;
+
+        private Camera camPerspective;
+        private Camera camTop;
+        private Camera camFront;
+        private Camera camBack;
+        private Camera camLeft;
+        private Camera camRight;
+        private Camera camOrtho;
+        public List<Camera> cams = new List<Camera>();
+        public Camera CurrentView;
+
+        //Init Some Operation Factors
+        public double RotateDeltaFactor =0.003D;
+        public const double panDeltaFactor = 0.002D;
+        public double zoomDeltaFactor = 0.1D;
+        private Vector2 mouseLastPosition;
+
+        private float lastZoomData = 0;
+
+        public bool FixZaxisRotation { get; set; } = false;
+
+        public CamController(IApp app,float dis=1000)
+        {
+            this.app = app;
+            this.window = this.app.window;
+            this.app.is2D = false;
+            ResetCameras(dis);
+            HandleWindow();
+        }
+        protected void HandleWindow()
+        {
+            this.app.window.Load += Window_Load;
+            this.app.window.RenderFrame += Window_RenderFrame;
+            this.app.window.UpdateFrame += Window_UpdateFrame;
+            this.app.window.MouseMove += new EventHandler<OpenTK.Input.MouseMoveEventArgs>(Mouse_Move);
+            this.app.window.MouseWheel += new EventHandler<OpenTK.Input.MouseWheelEventArgs>(Mouse_Wheel);
+            this.app.window.Resize += new EventHandler<EventArgs>(Window_Resize);
+            this.app.window.MouseDown += Mouse_Down;
+        }
+        public void ResetCameras(float dis=1000)
+        {
+            this.w = this.window.Width;
+            this.h = this.window.Height;
+            this.camPerspective = new Camera(w, h, new Vector3(-dis, -dis, dis), new Vector3(0.0F, 0.0F, 0.0F));
+            this.camTop = new Camera(w, h, new Vector3(0.0F, 0.0F, dis), new Vector3(0.0F, 0.0F, 0.0F));
+            this.camTop.Set2DProperties();
+            this.camFront = new Camera(w, h, new Vector3(0.0F, -dis, 0.0F), new Vector3(0.0F, 0.0F, 0.0F));
+            this.camTop.Set2DProperties();
+            this.camBack = new Camera(w, h, new Vector3(0.0F, dis, 0.0F), new Vector3(0.0F, 0.0F, 0.0F));
+            this.camBack.Set2DProperties();
+            this.camLeft = new Camera(w, h, new Vector3(-dis, 0.0F, 0.0F), new Vector3(0.0F, 0.0F, 0.0F));
+            this.camLeft.Set2DProperties();
+            this.camRight = new Camera(w, h, new Vector3(dis, 0.0F, 0.0F), new Vector3(0.0F, 0.0F, 0.0F));
+            this.camRight.Set2DProperties();
+            this.camOrtho = new Camera(w, h, new Vector3(-dis, -dis, dis), new Vector3(0.0F, 0.0F, 0.0F));
+            this.camOrtho.Set3DProperties(false);
+            this.cams.Add(camPerspective);
+            this.cams.Add(camTop);
+            this.cams.Add(camFront);
+            this.cams.Add(camBack);
+            this.cams.Add(camLeft);
+            this.cams.Add(camRight);
+            this.cams.Add(camOrtho);
+
+            this.Perspective();
+            //this.CurrentView.SetPerspective(false);
+            //this.Right();
+            //this.Front();
+            //this.Top();
+            //this.Ortho();
+        }
+
+        //Override based window functions
+        protected void Window_Load(Object sender, EventArgs e)
+        {
+
+            GL.ClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+            GL.Enable(EnableCap.Fog);
+            GL.Fog(FogParameter.FogColor, new float[4] { 0.0f, 0.0f, 0.0f, 1.0f });//Same color as clear.
+            GL.Fog(FogParameter.FogMode, (int)FogMode.Linear);
+            GL.Fog(FogParameter.FogEnd, (float)(4096 / 3.0f));
+  
+        }
+
+        protected void Window_UpdateFrame(object sender, FrameEventArgs e)
+        {
+
+        }
+
+        protected void Window_RenderFrame(object sender, FrameEventArgs e)
+        {
+            CurrentView.Update(this.app);
+        }
+
+        protected void Window_Resize(object sender, EventArgs e)
+        {
+            GL.Viewport(0, 0, this.window.Width, this.window.Height);
+            //Refresh all cameras
+            foreach (Camera cam in cams)
+            {
+                cam.Resize(this.window.Width, this.window.Height);
+            }
+        }
+
+        private void Mouse_Move(object sender, OpenTK.Input.MouseMoveEventArgs e)
+        {
+            if (e.Mouse.MiddleButton == OpenTK.Input.ButtonState.Pressed)
+            {
+                bool noRotation = true;
+                if (this.CurrentView == this.camPerspective)
+                {
+                    noRotation = false;
+                }
+
+                float dX = e.X - mouseLastPosition.X;
+                float dY = e.Y - mouseLastPosition.Y;
+
+                //this.CurrentView.RotateAroundLookAt(dX, dY, RotateDeltaFactor);
+                this.CurrentView.RotateAroundTarget(dX, dY, RotateDeltaFactor,FixZaxisRotation);
+                mouseLastPosition = new Vector2(e.X, e.Y);
+
+            }
+            else if (e.Mouse.RightButton == OpenTK.Input.ButtonState.Pressed)
+            {
+                var dX = e.XDelta;
+                var dY = e.YDelta;
+                this.CurrentView.Pan(dX,dY, false,panDeltaFactor);
+                mouseLastPosition = new Vector2(e.X, e.Y);
+            }
+
+        }
+        private void Mouse_Down(object sender, OpenTK.Input.MouseButtonEventArgs e)
+        {
+
+
+            mouseLastPosition = new Vector2(e.X, e.Y);
+
+        }
+        private void Mouse_Wheel(object sender, OpenTK.Input.MouseWheelEventArgs e)
+        {
+            double signum = 0.0D;
+            if (Math.Abs(-e.ValuePrecise) == 1)
+            {
+                signum = (double)(e.ValuePrecise);
+            }
+            else
+            {
+                signum = (double)Math.Sign((float)(this.lastZoomData+e.ValuePrecise));
+            }
+            this.CurrentView.Zoom(signum, zoomDeltaFactor);
+            this.lastZoomData = -e.ValuePrecise;
+        }
+
+        
+
+        //depreciated 待处理：欧拉角
+        public void Rotate(float dX,float dY)
+        {
+            this.CurrentView.Yaw += -(float)(dX * RotateDeltaFactor);
+            this.CurrentView.Pitch -= -(float)(dY * RotateDeltaFactor);
+        }
+
+        public void DrawSystem(IApp app, float len)
+        {
+            app.PushStyle();
+            this.DrawGrid(app, len, 20.0F);
+            app.StrokeWeight(2.0F);
+            app.Stroke(-65536);
+            app.Line(0.0F, 0.0F, 0.0F, len, 0.0F, 0.0F);
+            app.Stroke(-16711936);
+            app.Line(0.0F, 0.0F, 0.0F, 0.0F, len, 0.0F);
+            app.Stroke(-16776961);
+            app.Line(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, len);
+            app.PopStyle();
+        }
+
+        //Shift cameras
+        public void Top()
+        {
+            this.CurrentView = this.cams[1];
+
+        }
+        public void Right()
+        {
+            this.CurrentView = this.cams[5];
+
+        }
+        public void Front()
+        {
+            this.CurrentView = this.cams[2];
+
+        }
+
+        public void Perspective()
+        {
+            this.CurrentView = this.cams[0];
+            this.CurrentView.Update(this.app);
+        }
+        public void Ortho()
+        {
+            CurrentView = this.cams[6];
+        }
+
+        //Draw Grid and System
+
+        private void DrawGrid(IApp app, float len, float num)
+        {
+            
+            float weight =0.5F;
+            int gray = 100;
+            len *= 2.0F;
+            float step = len / num;
+            float start = -len / 2.0F;
+
+
+            for (int i = 0; (float)i <= num; ++i)
+            {
+                app.PushStyle(false);
+                app.Stroke(gray);
+                app.StrokeWeight(weight);
+                app.Line(start, (float)i * step + start, 0.0F, len + start, (float)i * step + start, 0.0F);
+                app.Line((float)i * step + start, start, 0.0F, (float)i * step + start, len + start, 0.0F);
+
+                app.PopStyle();
+
+                app.PushStyle(false);
+                app.Stroke(gray+100);
+                float step2 = step / 5.0F;
+                app.StrokeWeight(weight/4);
+                if (i != num)
+                {
+                    for (int j = 1; j < 5; j++)
+                    {
+                        app.Line(start, (float)i * step + j * step2 + start, 0.0F, len + start, (float)i * step + j * step2 + start, 0.0F);
+                        app.Line((float)i * step + j * step2 + start, start, 0.0F, (float)i * step + j * step2 + start, len + start, 0.0F);
+                    }
+                }
+
+                app.PopStyle();
+            }
+        }
+
+
+    }
+}
---
+++ Examples/Camera.cs
@@ -0,0 +1,527 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+
+namespace Flowing
+{
+    // This is the camera class as it could be set up after the tutorials on the website.
+    // It is important to note there are a few ways you could have set up this camera.
+    // For example, you could have also managed the player input inside the camera class,
+    // and a lot of the properties could have been made into functions.
+
+    // TL;DR: This is just one of many ways in which we could have set up the camera.
+    // Check out the web version if you don't know why we are doing a specific thing or want to know more about the code.
+    public class Camera
+    {
+        private IApp lastApp;
+        public bool perspective;
+        private double fovy;
+        private double far;
+        private double near;
+
+        private float frustumNear;
+        private float frustumFar;
+        private float frustumBottom;
+        private float frustumTop;
+        private float frustumLeft;
+        private float frustumRight;
+
+        private bool leftHandSystem;
+        // Those vectors are directions pointing outwards from the camera to define how it rotated.
+        private Vector3 _front = -Vector3.UnitZ;
+
+        private Vector3 _up = Vector3.UnitY;
+
+        private Vector3 _right = Vector3.UnitX;
+
+        // Rotation around the X axis (radians)
+        private float _pitch;
+
+        // Rotation around the Y axis (radians)
+        private float _yaw = -MathHelper.PiOver2; // Without this, you would be started rotated 90 degrees right.
+
+        // The field of view of the camera (radians)
+        private float _fov = MathHelper.PiOver2;
+        public int w;
+        public int h;
+        public Vector3 target;
+
+        private bool updateViewMatrix;
+        private bool updateProjectionMatrix;
+        private bool updateViewProjectionMatrix;
+
+        private Matrix4 viewMatrix;
+        private Matrix4 projectionMatrix;
+        public bool is2D;
+
+        public Camera(int w, int h, Vector3 position,Vector3 target)
+        {
+            iniCoordinateSystem(position, target);
+            this.w = w;
+            this.h = h;
+            AspectRatio = this.w/this.h;
+            this.fovy = 1.0471975511965976D;
+            this.far = 1000.0D;
+            this.near = 1000.0D;
+            this.frustumNear = 1.0f;
+            this.frustumFar = 2.0f;
+            this.frustumLeft = -0.5f;
+            this.frustumRight = 0.5f;
+            this.frustumTop = 0.5f;
+            this.frustumBottom = -0.5f;
+            this.perspective = true;
+            this.updateViewMatrix = true;
+            this.updateProjectionMatrix = true;
+            this.updateViewProjectionMatrix = true;
+            this.leftHandSystem = false;
+            this.is2D = false;
+        }
+        private void iniCoordinateSystem(Vector3 position,Vector3 target)
+        {
+            Vector3 zz = target - position;
+            Vector3 planxz;
+            if (zz.X == 0.0D && zz.Y == 0.0D)
+            {
+                planxz = Vector3.Cross(Vector3.UnitY, zz);
+            }
+            else
+            {
+                planxz = Vector3.Cross(Vector3.UnitZ, zz);
+            }
+            this.iniCoordinateSystem(position,zz, planxz);
+        }
+
+        private void iniCoordinateSystem(Vector3 position,Vector3 z, Vector3 planxz)
+        {
+            this.Position = position;
+            this._front = z.Normalized();
+            this._right = planxz;
+            this._up = Vector3.Cross(z, this._right).Normalized();
+            this._right = Vector3.Cross(this._front, this._up).Normalized();
+            if (this.target == null)
+            {
+                this.target = position + z;
+            }
+            this.UpdateViewMatrix();
+        }
+        public void Resize(int w, int h)
+        {
+            if (w != 0)
+            {
+                this.w = w;
+            }
+            if (h != 0)
+            {
+                this.h = h;
+            }
+            this.AspectRatio = (float)this.w / (float)this.h;
+        }
+
+        public void Set2DProperties()
+        {
+            this.is2D = true;
+            this.perspective = false;
+        }
+        public void Set3DProperties(bool perspective)
+        {
+            this.perspective = perspective;
+            this.is2D = false;
+        }
+        public void Set2D(bool is2D)
+        {
+            this.is2D = is2D;
+            this.UpdateProjectionMatrix();
+        }
+        public void SetPerspective(bool perspective)
+        {
+            if (!this.is2D)
+            {
+                this.perspective = perspective;
+            }
+            this.UpdateProjectionMatrix();
+        }
+        // The position of the camera
+        public Vector3 Position { get; set; }
+
+        // This is simply the aspect ratio of the viewport, used for the projection matrix.
+        public float AspectRatio {get; set; }
+
+        public Vector3 Front {
+            get => _front;
+            set
+            {
+                _front = value;
+            }
+        }
+
+        public Vector3 Up { 
+            get => _up;
+            set
+            {
+                _up = value;
+            }
+        }
+
+        public Vector3 Right
+        { 
+            get => _right;
+            set
+            {
+                _right=value;
+            }
+        }
+
+        // We convert from degrees to radians as soon as the property is set to improve performance.
+        public float Pitch
+        {
+            get => MathHelper.RadiansToDegrees(_pitch);
+            set
+            {
+                // We clamp the pitch value between -89 and 89 to prevent the camera from going upside down, and a bunch
+                // of weird "bugs" when you are using euler angles for rotation.
+                // If you want to read more about this you can try researching a topic called gimbal lock
+                var angle = MathHelper.Clamp(value, -89f, 89f);
+                _pitch = MathHelper.DegreesToRadians(angle);
+                UpdateVectors();
+                //this.UpdateViewMatrix();
+                //this.UpdateProjectionMatrix();
+            }
+        }
+
+        // We convert from degrees to radians as soon as the property is set to improve performance.
+        public float Yaw
+        {
+            get => MathHelper.RadiansToDegrees(_yaw);
+            set
+            {
+                _yaw = MathHelper.DegreesToRadians(value);
+                UpdateVectors();
+                //this.UpdateViewMatrix();
+                //this.UpdateProjectionMatrix();
+            }
+        }
+
+        // The field of view (FOV) is the vertical angle of the camera view.
+        // This has been discussed more in depth in a previous tutorial,
+        // but in this tutorial, you have also learned how we can use this to simulate a zoom feature.
+        // We convert from degrees to radians as soon as the property is set to improve performance.
+        public float Fov
+        {
+            get => (float)MathHelper.RadiansToDegrees(fovy);
+            set
+            {
+                var angle = MathHelper.Clamp(value, 1f, 90f);
+                fovy = MathHelper.DegreesToRadians(angle);
+                UpdateVectors();
+                //this.UpdateViewMatrix();
+                //this.UpdateProjectionMatrix();
+            }
+        }
+
+        // Get the view matrix using the amazing LookAt function described more in depth on the web tutorials
+        public void GetViewMatrix()
+        {
+
+            this.viewMatrix= Matrix4.LookAt(Position, target, _up);
+
+        }
+
+        // Get the projection matrix using the same method we have used up until this point
+        public void GetProjectionMatrix()
+        {
+            this.getFrustumPerspective();
+            this.projectionMatrix= Matrix4.CreatePerspectiveFieldOfView((float)fovy, AspectRatio, 0.01f, 100000f);
+            //this.projectionMatrix=Matrix4.CreatePerspectiveOffCenter(this.frustumLeft, this.frustumRight, this.frustumBottom, this.frustumTop, this.frustumNear, this.frustumFar);
+        }
+
+        public void UpdateViewMatrix()
+        {
+            this.updateViewMatrix = true;
+            this.updateViewProjectionMatrix = true;
+        }
+
+        public void UpdateProjectionMatrix()
+        {
+            this.updateProjectionMatrix = true;
+            this.updateViewProjectionMatrix = true;
+        }
+        private void checkMatrixUpdate()
+        {
+            if (this.updateViewMatrix)
+            {
+                this.GetViewMatrix();
+                this.updateViewMatrix = false;
+            }
+
+            if (this.updateProjectionMatrix)
+            {
+                this.GetProjectionMatrix();
+                this.updateProjectionMatrix = false;
+            }
+
+        }
+        private void getFrustumPerspective()
+        {
+            double aspect = this.w / this.h;
+            double halfHeight = this.h / 2;
+            double tan = Math.Tan(this.fovy / 2);
+            double cameraZ = halfHeight / tan;
+            double zNear = cameraZ / this.near;
+            double zFar = cameraZ * this.far;
+            double h = halfHeight / this.near;
+            double w = h * aspect;
+            this.frustumLeft = (float)-w;
+            this.frustumRight = (float)w;
+            this.frustumBottom = (float)-h;
+            this.frustumTop = (float)h;
+            this.frustumNear = (float)zNear;
+            this.frustumFar = (float)zFar;
+
+            if (!this.perspective)
+            {
+                float dist = (this.target - this.Position).Length;
+                this.frustumLeft *= dist;
+                this.frustumRight *= dist;
+                this.frustumBottom *= dist;
+                this.frustumTop *= dist;
+            }
+            double dx = this.frustumRight - this.frustumLeft;
+            double dy = this.frustumTop - this.frustumBottom;
+
+
+            if (Math.Abs(dx) > 0.001 || Math.Abs(dy) > 0.001)
+            {
+                if (dx >= dy)
+                {
+                    double dY = dx * this.h / this.w;
+                    frustumTop = (float)(frustumBottom + dY);
+                }
+                else
+                {
+                    double dX = dy * this.w / this.h;
+                    frustumRight = (float)(frustumLeft + dX);
+                }
+            }
+
+
+        }
+
+        // This function is going to update the direction vertices using some of the math learned in the web tutorials.  待解决：基于欧拉角的旋转
+        private void UpdateVectors()
+        {
+            float dis = (this.target - this.Position).Length;
+            // First, the front matrix is calculated using some basic trigonometry.
+            _front.X = (float)(Math.Cos(_pitch) * Math.Cos(_yaw));
+            _front.Y = (float)Math.Sin(_pitch);
+            _front.Z = (float)(Math.Cos(_pitch) * Math.Sin(_yaw));
+
+            // We need to make sure the vectors are all normalized, as otherwise we would get some funky results.
+            _front = Vector3.Normalize(_front);
+
+            this.target = this.Position + _front * dis;
+            //target = Position + _front;
+            // Calculate both the right and the up vector using cross product.
+            // Note that we are calculating the right from the global up; this behaviour might
+            // not be what you need for all cameras so keep this in mind if you do not want a FPS camera.
+            _right = Vector3.Normalize(Vector3.Cross(_front, _up));
+            _up = Vector3.Normalize(Vector3.Cross(_right, _front));
+        }
+
+
+        public void Zoom(double data,double zoomDeltaFactor)
+        {
+
+
+            if (data != 0.0D)
+            {
+                double dist = 500.0D;
+                if (this.target != null)
+                {
+                    dist = (this.target - this.Position).Length;
+
+                }
+
+                this.MoveIn(data * dist * zoomDeltaFactor);
+
+            }
+        }
+
+        public void MoveIn(double dist)
+        {
+            Vector3 lookDirection = (this.target - this.Position).Normalized();
+
+            this.Position += (float)dist * lookDirection;
+            this.UpdateViewMatrix();
+            if (!this.perspective)
+            {
+                this.UpdateProjectionMatrix();
+            }
+
+        }
+
+        public void Pan(double dx, double dy, bool onXZ,double panDeltaFactor)
+        {
+            double DistToOrign = (this.target-this.Position).Length;
+            if (onXZ)
+            {
+
+                dx *= -panDeltaFactor * DistToOrign;
+                dy *= panDeltaFactor * DistToOrign;
+                this.Position = new Vector3(this.Position.X + (float)dx, this.Position.Y + (float)dy, this.Position.Z);
+
+            }
+            else
+            {
+
+                dx *= -panDeltaFactor * DistToOrign;
+                dy *= panDeltaFactor * DistToOrign;
+
+                Vector3 movex = (float)dx * this.Right;
+                Vector3 movey = (float)dy * this.Up;
+
+                Vector3 move = movex + movey;
+                //move.Z = 0.0F;
+                this.Position += move;
+                this.target += move;
+            }
+            this.UpdateViewMatrix();
+        }
+
+        public void RotateAroundTarget(double dx, double dy, double rotateDeltaFactor, bool FixZaxisRotation = false)
+        {
+            bool noRotation = true;
+            int dir;
+            if (!is2D)
+            {
+                noRotation = false;
+            }
+
+            if (!noRotation && this.target != null)
+            {
+                dir = this.leftHandSystem ? 1 : -1;
+
+                double ang1 = (double)(dir * dx) * rotateDeltaFactor;
+                double ang2 = (double)dy * rotateDeltaFactor;
+                if (!FixZaxisRotation)
+                {
+                    HorizontalTransform(ang1);
+                    VerticalTransform(ang2);
+                }
+                else
+                {
+                    RotateLookAtHorizontal(ang1);
+                    RotateLookAtVertical(-ang2);
+                }
+            }
+        }
+
+        public void RotateLookAtHorizontal(double angle)
+        {
+            if (this.target != null)
+            {
+                this.Rotate(this.target, new Vector3(0.0F,0.0F,1.0F), angle);
+            }
+        }
+        public void RotateLookAtVertical(double angle)
+        {
+            if (this.target != null)
+            {
+    
+                this.Rotate(this.target, new Vector3(this._right.X,this._right.Y, this._right.Z), angle);
+            }
+        }
+
+        public void VerticalTransform(double angle)
+        {
+            Vector3 rotateAxis = Vector3.Cross(this.Position, this._up);
+
+            this.Position = RotateVector(this.Position, rotateAxis, angle);
+            this._front = RotateVector(this._front, rotateAxis, angle);
+
+
+            //update the up direction
+            Vector3 newUpDirection = Vector3.Cross(this.Front, rotateAxis);
+            newUpDirection.Normalize();
+            this.Up = newUpDirection;
+
+            this.UpdateViewMatrix();
+        }
+
+        public void HorizontalTransform( double angle)
+        {
+            Vector3 rotateAxis = this.Up;
+
+            this.Position = RotateVector(this.Position, rotateAxis, angle);
+            this._front = RotateVector(this._front, rotateAxis, angle);
+            this._right = Vector3.Cross(this._front, rotateAxis).Normalized();
+            this._up = Vector3.Cross(this._right, this._front).Normalized();
+
+            this.UpdateViewMatrix();
+        }
+        public void Rotate(Vector3 center, Vector3 axis, double angle)
+        {
+            this.Position = RotateVector(this.Position, center, axis, angle);
+            this.target = RotateVector(this.target, center, axis, angle);
+            this._up = RotateVector(this._up, axis, angle);
+            this._front = RotateVector(this._front, axis, angle);
+            this._right = RotateVector(this._right, axis, angle);
+
+            this.UpdateViewMatrix();
+        }
+
+        public Vector3 RotateVector(Vector3 based, Vector3 center, Vector3 axis, double angle)
+        {
+            return center == based ? based : (RotateVector(based - center, axis, angle) + center);
+        }
+        private Vector3 RotateVector(Vector3 based, Vector3 axis, double angle)
+        {
+
+            Matrix4 ro = Matrix4.CreateFromAxisAngle(axis, (float)angle);
+            Vector3 newV = Vector4.Transform(new Vector4(based, 0.0f), ro).Xyz;
+            return newV;
+        }
+
+       
+
+        public void Update(IApp app)
+        {
+
+            this.UpdateProjectionMatrix();
+            this.UpdateViewMatrix();
+
+            this.lastApp = app;
+            checkMatrixUpdate();
+
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref this.viewMatrix);
+
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+
+
+            if (this.perspective)
+            {
+
+                GL.LoadMatrix(ref this.projectionMatrix);
+                //GL.Frustum((float)this.frustumLeft, this.frustumRight, this.frustumBottom,this.frustumTop, (float)this.frustumNear, (float)this.frustumFar);
+            }
+            else
+            {
+                //GL.Ortho(frustumLeft, frustumRight, frustumBottom, frustumTop, -frustumFar, frustumFar);
+                Matrix4 ortho = Matrix4.CreateOrthographic(frustumRight-frustumLeft, frustumTop-frustumBottom, (float)-this.frustumFar, (float)this.frustumFar);
+                GL.LoadMatrix(ref ortho);
+            }
+
+            GL.MatrixMode(MatrixMode.Modelview);
+            //GL.LoadIdentity();
+        }
+
+
+
+    }
+}
